8496be32c42764661d8daf80f79786303b9541a3
TOMASKOSSSNTB\Tomaskosss <240906@vutbr.cz>
2022-04-28 14:09:26 +0200

    Add project files.

---
+++ void funckia spojeni so starym cvikom/void funckia spojeni so starym cvikom.cpp
@@ -0,0 +1,76 @@
+﻿// void funckia spojeni so starym cvikom.cpp : Defines the entry point for the application.
+//
+
+#include "void funckia spojeni so starym cvikom.h"
+
+using namespace std;
+void soucet(double a[2], double b[2], double c[2])
+{
+	c[0] = a[0] + b[0];
+	c[1] = a[1] + b[1];
+}
+void odecet(double a[2], double b[2], double c[2])
+{
+	c[0] = a[0] - b[0];
+	c[1] = a[1] - b[1];
+}
+void tisk(double c[2])
+{
+	cout << c[0] << "	" << c[1]<<"j"<<endl;
+
+}
+void tisk2(double c[2])
+{
+	cout << c[0] << "	" << c[1] << "j";
+
+}
+void soucin(double a[2], double b[2], double c[2])
+{
+	double ulozenka;
+	ulozenka = c[0];
+	c[0] = sqrt(pow(c[1], 2) + pow(ulozenka, 2));
+	c[1] = atan(c[1] / c[0]) * (180.0 / 3.141);
+}
+void znamenko(char x, double a[2], double b[2], double c[2])
+{
+	if (x == '+')
+	{
+		soucet(a, b, c);
+	}
+	if (x == '-')
+	{
+		odecet(a, b, c);
+	}
+	
+	if (x != '+' && x != '-')
+	{
+		cout << "zle zadane znamienko" << endl;
+		exit(0);
+	}
+	
+
+}
+int main()
+{
+	double a[2];
+	double b[2];
+	double c[2];
+	cout << "Zadaj komplexne cislo 1: " << endl;
+	cin >> a[0] >> a[1];
+	cout << "Zadaj komplexne cislo 2: " << endl;
+	cin >> b[0] >> b[1];
+	cout << " 1.zadane cislo:  " << a[0] << " " << a[1] << "i " << endl;
+	cout << " 2.zadane cislo:  " << b[0] << " " << b[1] << "i " << endl;
+	cout << "Zvolte operaci + alebo - " << endl;
+	char x;
+	cin >> x; 
+	znamenko(x, a, b, c);
+	tisk(c);
+	soucin(a, b, c);
+	tisk2(c);
+	
+
+
+
+	return 0;
+}
---
+++ void funckia spojeni so starym cvikom/void funckia spojeni so starym cvikom.h
@@ -0,0 +1,8 @@
+﻿// void funckia spojeni so starym cvikom.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
